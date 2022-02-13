f03a7b0ba1ea9cf830ef6569e6979eb868a9d39a
Gerardo Velasquez <gerardo.jose2011@yahoo.es>
2022-02-12 22:25:14 -0600

    Avances genericos en el Edit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,32 +1,53 @@
 <?php
-    namespace Controller;
-    use MVC\Router;
-    use Model\User;
-    class UserController{
-        public static function getUser(Router $router){
-            $users = new User();
-            $usuarios=$users->getUser();
-            $router->render('home',['users'=>$usuarios]);
-        }
 
-        public static function getDeleteUser(Router $router){
-            $id = $_GET['id']??null;
-            $users = new User();
-            $usuarios=$users->getDeleteUser($id);
-            $router->render('delete',['users'=>$usuarios]);
-        }
-        
-        public static function postDeleteUser(Router $router){
-            $id = $_POST['id']??null;
-            $users = new User();
-            $users->postDeleteUser($id);
-            $router->redirect('/');
-        }
+namespace Controller;
 
-        public static function getUpdateUser(Router $router){
-            $id = $_GET['id']??null;
-            $users = new User();
-            $usuarios=$users->getUpdateUser($id);
-            $router->render('update',['users'=>$usuarios]);
-        }
+use MVC\Router;
+use Model\User;
+
+class UserController
+{
+    public static function getUser(Router $router)
+    {
+        $users = new User();
+        $usuarios = $users->getUser();
+        $router->render('home', ['users' => $usuarios]);
     }
+
+    public static function getDeleteUser(Router $router)
+    {
+        $id = $_GET['id'] ?? null;
+        $users = new User();
+        $usuarios = $users->getDeleteUser($id);
+        $router->render('delete', ['users' => $usuarios]);
+    }
+
+    public static function postDeleteUser(Router $router)
+    {
+        $id = $_POST['id'] ?? null;
+        $users = new User();
+        $users->postDeleteUser($id);
+        $router->redirect('/');
+    }
+
+    public static function getEditUser(Router $router)
+    {
+        $id = $_GET['id'] ?? null;
+        $users = new User();
+        $usuarios = $users->getEditUser($id);
+        $router->render('edit', ['users' => $usuarios]);
+    }
+
+    public static function postEditUser(Router $router)
+    {
+        $id = $_POST['id'] ?? null;
+        $firstName = $_POST['first_name'] ?? null;
+        $lastName = $_POST['last_name'] ?? null;
+        $email = $_POST['email'] ?? null;
+        $gender = $_POST['gender'] ?? null;
+        $city = $_POST['city'] ?? null;
+        $users = new User();
+        $users->postEditUser($id, $firstName, $lastName, $email, $gender, $city);
+        $router->redirect('/');
+    }
+}
--- src/Model/User.php
+++ src/Model/User.php
@@ -1,33 +1,50 @@
 <?php
-    namespace Model;
-class User extends ActiveRecord{
-    public function getUser(){
-        $query='SELECT * FROM usuarios';
-        $result=self::$db->query($query);
-        if($result->num_rows>0){
-            $valor=mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+namespace Model;
+
+class User extends ActiveRecord
+{
+    public function getUser()
+    {
+        $query = 'SELECT * FROM usuarios';
+        $result = self::$db->query($query);
+        if ($result->num_rows > 0) {
+            $valor = mysqli_fetch_all($result, MYSQLI_ASSOC);
             return $valor;
         }
         return [];
     }
 
-    public function getDeleteUser($id){
-        $query='SELECT * FROM usuarios WHERE id='.$id;
-        $result=self::$db->query($query);
-        if($result->num_rows>0){
-            $valor=$result->fetch_assoc();
+    public function getDeleteUser($id)
+    {
+        $query = 'SELECT * FROM usuarios WHERE id=' . $id;
+        $result = self::$db->query($query);
+        if ($result->num_rows > 0) {
+            $valor = $result->fetch_assoc();
             return $valor;
         }
         return [];
     }
-    /*
-    public function getUpdateUser($id){
-        $query='UPDATE usuarios SET '
-    }*/
 
-    public function postDeleteUser($id){
-        $query='DELETE FROM usuarios WHERE id='.$id;
+    public function postDeleteUser($id)
+    {
+        $query = 'DELETE FROM usuarios WHERE id=' . $id;
         self::$db->query($query);
     }
 
+    public function getEditUser($id)
+    {
+        $query = 'SELECT * FROM usuarios WHERE id=' . $id;
+        $result = self::$db->query($query);
+        if ($result->num_rows > 0) {
+            $valor = $result->fetch_assoc();
+            return $valor;
+        }
+        return [];
+    }
+
+    public function postEditUser($id, $firstName, $lastName, $email, $gender, $city)
+    {
+        $query = 'UPDATE usuarios SET id=' . $id . ', first_name=' . $firstName . ', last_name=' . $lastName . ', email=' . $email . ', gender=' . $gender . ', city=' . $city;
+    }
 }
